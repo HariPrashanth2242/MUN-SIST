448d1be786037525d4ef42a7b76c72ddac969130
HariPrashanth2242 <hariprashanth8989@gmail.com>
2025-03-15 20:48:28 +0530

    bug

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -25,20 +25,19 @@ export async function POST(req: Request) {
 
     // ✅ 4. Submit the form with additional realistic headers
     const response = await fetch(googleFormURL, {
-      method: "POST",
+      method: 'POST',
       body: params,
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Referer": "https://docs.google.com/forms/d/e/1FAIpQLScNJhGtcfiscV4-r553336JR8XbZgzLCnYHYuTquh0pUUEt-Q/viewform", // Refers to the form page
-        "Origin": "https://docs.google.com", // The domain from which the form is hosted
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
       }
     });
+    
     
 
     // ✅ 5. Check for a successful submission
